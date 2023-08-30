cbda3facc3530ef3f43c73bb3ea82896c1bb192d
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2023-08-30 15:55:03 +0200

    `Encode`:  Add exprimental subtitle + OCR support

--- encode_framework/encode.py
+++ encode_framework/encode.py
@@ -1,16 +1,23 @@
 import re
+import shutil
 import subprocess
 from typing import Any, Literal, cast
 
-from vsmuxtools import AudioFile, AudioTrack, Chapters
+from babelfish import Language  # type:ignore[import]
+from magic import Magic
+from vsmuxtools import AudioFile, AudioTrack, Chapters  # type:ignore[import]
 from vsmuxtools import Encoder as AudioEncoder  # type:ignore[import]
-from vsmuxtools import FFMpeg, HasTrimmer, VideoFile, ensure_path, qAAC, x265
+from vsmuxtools import (FFMpeg, HasTrimmer, SubTrack, VideoFile, ensure_path, frame_to_ms, qAAC,  # type:ignore[import]
+                        x265)
 from vsmuxtools.video.encoders import SupportsQP  # type:ignore[import]
 from vstools import (CustomError, CustomNotImplementedError, CustomRuntimeError, CustomValueError, FileNotExistsError,
                      FileType, SPath, SPathLike, vs)
 
+from .git import clone_git_repo
 from .logging import Log
 from .script import ScriptInfo
+from .types import TextSubExt, is_windows
+from .util import cargo_build, check_program_installed, run_cmd
 
 __all__: list[str] = [
     "Zones",
@@ -39,6 +46,12 @@ class Encoder:
 
     audio_tracks: list[AudioTrack] = []
     """A list of all audio tracks."""
+
+    subtitle_files: list[SPath] = []
+    """A list of all the subtitle source files."""
+
+    subtitle_tracks: list[SubTrack] = []
+    """A list of all subtitle tracks."""
 
     chapters: Chapters | None = None
     """Chapters obtained from the m2ts playlist or elsewhere."""
@@ -233,7 +246,6 @@ class Encoder:
                                 I'm aware I said it would never re-encode it.
         :param track_args:      Keyword arguments for the track.
         """
-        import shutil
         from itertools import zip_longest
 
         from vsmuxtools import FLAC, Sox, do_audio, frames_to_samples, is_fancy_codec, make_output
@@ -410,6 +422,159 @@ class Encoder:
         except Exception as e:
             Log.error(str(e), self.__clean_acopy, CustomValueError)
 
+    def find_sub_files(self, dgi_path: SPathLike | None = None) -> list[SPath]:
+        """
+        Find accompanying DGIndex(NV) demuxed pgs tracks.
+
+        If input file is not a dgi file, it will throw an error.
+        """
+        if dgi_path is not None:
+            dgi_file = SPath(dgi_path)
+        else:
+            dgi_file = self.script_info.src_file
+
+        if not dgi_file.to_str().endswith(".dgi"):
+            Log.error("Input file is not a dgi file, not returning any subs", self.find_sub_files)
+            return []
+
+        mg = Magic(mime=True)
+
+        # TODO: make sure this checks the stem of the filename so it doesn't grab the wrong sup's.
+        for f in dgi_file.glob(f"*.*"):
+            get_mime = mg.from_file(f.to_str())
+
+            if get_mime == "application/octet-stream":
+                self.subtitle_files += [f]
+            elif get_mime == "video/mpeg" and f.to_str().endswith(".sub"):
+                self.subtitle_files += [f]
+            elif f.to_str().endswith(TextSubExt):
+                self.subtitle_files += [f]
+
+        return self.subtitle_files
+
+    def process_subs(
+        self, subtitle_files: SPathLike | list[SPath] | None = None,
+        langs: list[Language] = [Language("eng"), Language("jpn")],
+        sub_delay: int | None = None, strict: bool = False
+    ) -> list[SubTrack]:
+        """
+        OCR and process any files that are found.
+        """
+        if subtitle_files is not None and not isinstance(subtitle_files, list):
+            subtitle_files = [SPath(subtitle_files)]
+
+        sub_files = subtitle_files or self.subtitle_files
+
+        if not sub_files:
+            return sub_files
+
+        Log.info("The following subtitle files were found!", self.process_subs)
+
+        for sub in sub_files:
+            try:
+                Log.info(f"    - \"{SPath(sub).name}\"", self.process_subs)
+            except (AttributeError, ValueError) as e:
+                Log.warn(f"    - Could not determine track name!\n{e}", self.process_subs)
+
+        if sub_delay is None:
+            sub_delay = frame_to_ms(self.script_info.src.trim[0], self.out_clip.fps, compensate=True)
+
+        try:
+            check_program_installed("tesseract", "https://codetoprosper.com/tesseract-ocr-for-windows/")
+        except FileNotFoundError as e:
+            Log.error(str(e), self.process_subs)
+
+            Log.warn("Will still convert the files to tracks...", self.process_subs)
+
+            self.subtitle_tracks = list([
+                SubTrack(sub, default=not bool(i), delay=int(sub_delay))
+                for i, sub in enumerate(self.subtitle_files)
+            ])
+
+            return self.subtitle_tracks
+
+        try:
+            clone_git_repo("https://github.com/tesseract-ocr/tessdata_best.git")
+        except Exception as e:
+            if not "exit code(128)" in str(e):
+                Log.error(f"Some kind of error occurred while cloning the \"tessdata_best\" repo!\n{e}", self.process_subs)
+
+                return sub_files
+
+            pass
+
+        mg = Magic(mime=True)
+        proc_files = []
+
+        for i, sub in enumerate(sub_files):
+            sub_spath = SPath(sub)
+
+            Log.info(f"[{i + 1}/{len(sub_files)}] OCRing \"{sub_spath.name}\"...", self.process_subs)
+
+            if sub_spath.to_str().endswith(TextSubExt):
+                Log.info(
+                    f"[{i + 1}/{len(sub_files)}] \"{sub_spath.name}\" is a text-based "
+                    "subtitle format. Skipping OCR!", self.process_subs
+                )
+
+                proc_files += [sub_spath]
+
+                continue
+
+            get_mime = mg.from_file(SPath(sub).to_str())
+
+            if get_mime == "application/octet-stream":
+                from pgsrip import Options, Sup, pgsrip  # type:ignore
+
+                if pgsrip.rip(Sup(sub), Options(languages=langs, overwrite=not strict, one_per_lang=False)):
+                    Log.info(f"[{i + 1}/{len(sub_files)}] Done!", self.process_subs)
+                    proc_files += [sub_spath.with_suffix(".srt")]
+                else:
+                    Log.warn(
+                        f"An error occurred while OCRing \"{sub_spath.name}\"! Passing the original file instead...",
+                        self.process_subs
+                    )
+
+                    proc_files += [sub_spath]
+            elif get_mime == "video/mpeg" and sub_spath.to_str().endswith(".sub"):
+                idx = SPath(sub).with_suffix(".idx")
+                out = sub_spath.with_suffix(".srt")
+
+                try:
+                    if not check_program_installed("vobsubocr"):
+                        Log.error("\"vobsubocr\" could not be found! Attempting to build...", self.process_subs)
+
+                        if not check_program_installed("vcpkg"):
+                            repo = clone_git_repo("https://github.com/microsoft/vcpkg")
+
+                            if is_windows:
+                                run_cmd([repo / "bootstrap-vcpkg.bat"])
+                                run_cmd([repo / "vcpkg", "integrate", "install"])
+                            else:
+                                run_cmd([repo / "bootstrap-vcpkg.sh"])
+
+                        cargo_build("vobsubocr")
+
+                    run_cmd(["vobsubocr", "-l", langs[0], "-o", out, idx])
+
+                    proc_files += [out]
+                except Exception as e:
+                    Log.error(str(e), self.process_subs)
+
+                    proc_files += [sub_spath]
+            else:
+                Log.info(
+                    f"[{i + 1}/{len(sub_files)}] \"{sub_spath.name}\" is not a text-based "
+                    "subtitle format, but could not process. Not touching.", self.process_subs
+                )
+
+                proc_files += [sub_spath]
+
+        for i, sub in enumerate(sub_files):
+            self.subtitle_tracks += [SubTrack(sub, default=not bool(i), delay=int(sub_delay))]
+
+        return self.subtitle_tracks
+
     def encode_video(
         self,
         input_clip: vs.VideoNode | None = None,
@@ -502,7 +667,10 @@ class Encoder:
             if self.chapters:
                 Log.debug(f"   - [CHAPTERS] {self.chapters}", self.mux)
 
-        self.premux_path = SPath(mux(video_track, *self.audio_tracks, self.chapters, outfile=out_path))
+        self.premux_path = SPath(mux(
+            video_track, *self.audio_tracks, *self.subtitle_tracks,
+            self.chapters, outfile=out_path
+        ))
 
         Log.info(
             f"Final file \"{self.premux_path.name}\" output to "

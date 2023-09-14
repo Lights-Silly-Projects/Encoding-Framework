3857dd6c1fac3563075be06ba7932a802887a00f
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2023-09-14 08:51:30 -0700

    assert_truthy: return bool properly

--- encode_framework/util/checks.py
+++ encode_framework/util/checks.py
@@ -9,9 +9,7 @@ __all__: list[str] = [
 
 def assert_truthy(var: Any, caller: str | Callable[[Any], Any] | None = None) -> bool:
     """Asserts whether a given variable is truthy. If not, raises a CustomValue log error."""
-    try:
-        assert var
-    except AssertionError:
-        raise Log.error(f"The variable \"{var}\" is not truthy!", caller or assert_truthy, CustomValueError, 4)
+    if not (x := bool(var)):
+        Log.error(f"The variable \"{var}\" is not truthy!", caller or assert_truthy, CustomValueError, 4)
 
-    return True
+    return x
